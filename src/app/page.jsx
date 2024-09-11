7747fbfd2108ca4de1fe4dd7f62fa791f5a3df01
Yeji <yzii9841@gmail.com>
2024-09-11 23:06:41 +0900

    feat: align items in home

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,8 +1,8 @@
 import { Typography } from '@mui/material'
 export default function Home() {
   return (
-    <>
+    <div className="home">
       <Typography>© 2024 Yeji Jung. All rights reserved.</Typography>
-    </>
+    </div>
   )
 }
