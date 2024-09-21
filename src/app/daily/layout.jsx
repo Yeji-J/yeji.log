524698da32e06056620a51ad5ea44fa2cc9f95f5
Yeji <yzii9841@gmail.com>
2024-09-22 02:17:51 +0900

    fix: daily layout

--- src/app/daily/layout.jsx
+++ src/app/daily/layout.jsx
@@ -1,30 +1,34 @@
 'use client'
 import React from 'react'
 import { useParams } from 'next/navigation'
-import PlayArrowIcon from '@mui/icons-material/PlayArrow'
 import FormatListBulletedRoundedIcon from '@mui/icons-material/FormatListBulletedRounded'
+import Title from '@components/Title'
 import styled from '@emotion/styled'
-import '@styles/travelDetail.css'
 
 export default function Layout({ children }) {
   const params = useParams()
-  const date = params.date ? params.date : ''
 
   return (
     <div>
-      <div className="title-container">
-        <Title className="main">
-          <FormatListBulletedRoundedIcon />
-          <div>DAILY LOG</div>
-        </Title>
-      </div>
+      <TitleWrapper>
+        <Title icon={<FormatListBulletedRoundedIcon />} text="daily log" />
+      </TitleWrapper>
       {children}
     </div>
   )
 }
 
-const Title = styled.div`
+const TitleWrapper = styled.div`
   display: flex;
   align-items: center;
   gap: 0.5rem;
+
+  /* mobile : page title width 100% */
+  @media (max-width: 479px) {
+    flex-wrap: wrap;
+
+    & > *:first-child {
+      width: 100%;
+    }
+  }
 `
